bf3dd9f9f0eeb2a399b2d4b1768b3f4f7747d087
afredoherculano <alfredo.hsh@gmail.com>
2025-12-02 16:30:34 -0300

    feat: implement core models and register models in admin

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Aluno, Curso, Matricula
+
+admin.site.register(Aluno)
+admin.site.register(Curso)
+admin.site.register(Matricula)
--- escola/models.py
+++ escola/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 
 class Aluno(models.Model):
-    nome = models.CharField(max_length=30, null=False)
-    email = models.CharField(max_length=20, unique=True, null=False)
+    nome = models.CharField(max_length=40, null=False)
+    email = models.CharField(max_length=40, unique=True, null=False)
     cpf = models.CharField(max_length=11, unique=True, null=False)
     data_ingresso = models.DateField(null=False)
+
+    def __str__(self):
+        return self.nome
 
 
 STATUS_CURSO = [
@@ -19,6 +22,9 @@ class Curso(models.Model):
     valor_inscricao = models.FloatField(default=0.0, null=False)
     status = models.CharField(max_length=10, choices=STATUS_CURSO, default="ativo")
 
+    def __str__(self):
+        return self.nome
+
 
 STATUS_MATRICULA = [
     ("pago", "Pago"),
@@ -28,6 +34,9 @@ STATUS_MATRICULA = [
 class Matricula(models.Model):
     data = models.DateField(null=False)
     status = models.CharField(max_length=15, choices=STATUS_MATRICULA, default="pendente")
-    aluno_id = models.OneToOneField(Aluno, on_delete=models.CASCADE)
-    curso_id = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    aluno = models.OneToOneField(Aluno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.aluno.nome} - {self.curso.nome} - {self.get_status_display()}"
 
